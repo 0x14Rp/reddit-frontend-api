c0eadb7a8859f29ae1d27efa1e627514cb4c5edb
Rafael <elgondu@gmail.com>
2020-06-04 18:32:19 -0400

    add component login

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {LoginRequestPayload} from "./LoginRequestPayload";
+import {AuthService} from "../shared/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginRequestPayload: LoginRequestPayload;
+  loginForm: FormGroup;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  login() {
+    this.loginRequestPayload.username = this.loginForm.get('username').value;
+    this.loginRequestPayload.password = this.loginForm.get('password').value;
+
+    this.authService.login(this.loginRequestPayload).subscribe(data => {
+      console.log('Login successful');
+    });
+  }
+}
