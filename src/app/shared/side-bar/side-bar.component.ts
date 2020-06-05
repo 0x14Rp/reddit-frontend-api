94508bf1274be0be6437a47c71cb1be66e6249d8
Rafael <elgondu@gmail.com>
2020-06-05 18:27:38 -0400

    add side-bar component

---
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  goToCreateSubreddit() {
+
+  }
+
+  goToCreatePost() {
+
+  }
+}
---
+++ src/app/subreddit/subreddit.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {SubredditModel} from "./subreddit-model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubredditService {
+  constructor(private http: HttpClient) { }
+
+  getAllSubreddits(): Observable<Array<SubredditModel>> {
+    return this.http.get<Array<SubredditModel>>('http://localhost:8080/api/subreddit/');
+  }
+}
