3955046f58fbc73ce73866f746c1d2e2c84a97b7
Rafael <elgondu@gmail.com>
2020-06-05 18:29:03 -0400

    add post service and model for response backend

---
+++ src/app/shared/post-model.ts
@@ -0,0 +1,11 @@
+export class PostModel {
+  id: number;
+  postName: string;
+  url: string;
+  description: string;
+  voteCount: number;
+  userName: string;
+  subredditName: string;
+  commentCount: number;
+  duration: string;
+}
---
+++ src/app/shared/post.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {PostModel} from "./post-model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllPosts(): Observable<Array<PostModel>> {
+    return this.http.get<Array<PostModel>>('http://localhost:8080/api/posts/');
+  }
+}
