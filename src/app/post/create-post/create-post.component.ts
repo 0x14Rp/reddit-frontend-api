05dae05105a8fb785263437811da4ef2e09f9058
Rafael <elgondu@gmail.com>
2020-06-05 20:31:51 -0400

    add component create post

---
+++ src/app/post/create-post/create-post.component.ts
@@ -0,0 +1,61 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {throwError} from "rxjs";
+import {CreatePostPayload} from "./create-post-payload";
+import {Router} from "@angular/router";
+import {PostService} from "../../shared/post.service";
+import {SubredditService} from "../../subreddit/subreddit.service";
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css']
+})
+export class CreatePostComponent implements OnInit {
+
+  createPostForm: FormGroup;
+  postPayload: CreatePostPayload;
+  subreddits: Array<any>;
+
+  constructor(private router: Router, private postService: PostService,
+              private subredditService: SubredditService) {
+    this.postPayload = {
+      postName: '',
+      url: '',
+      description: '',
+      subredditName: ''
+    }
+  }
+
+  ngOnInit() {
+    this.createPostForm = new FormGroup({
+      postName: new FormControl('', Validators.required),
+      subredditName: new FormControl('', Validators.required),
+      url: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+    });
+    this.subredditService.getAllSubreddits().subscribe((data) => {
+      this.subreddits = data;
+    }, error => {
+      throwError(error);
+    });
+  }
+
+  createPost() {
+    this.postPayload.postName = this.createPostForm.get('postName').value;
+    this.postPayload.subredditName = this.createPostForm.get('subredditName').value;
+    this.postPayload.url = this.createPostForm.get('url').value;
+    this.postPayload.description = this.createPostForm.get('description').value;
+
+    this.postService.createPost(this.postPayload).subscribe((data) => {
+      this.router.navigateByUrl('/');
+    }, error => {
+      throwError(error);
+    })
+  }
+
+  discardPost() {
+    this.router.navigateByUrl('/');
+  }
+
+}
