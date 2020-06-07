b5542dbc651647862ed6faf1ad02c72eb5e80496
Rafael <elgondu@gmail.com>
2020-06-07 18:03:47 -0400

    refactor app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,61 +11,61 @@ import {NgxWebstorageModule} from "ngx-webstorage";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
 import {TokenInterceptor} from "./TokenInterceptor";
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from './home/home.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import { PostTitleComponent } from './shared/post-title/post-title.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
-import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
-import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
-import { CreatePostComponent } from './post/create-post/create-post.component';
-import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subreddits.component';
+import {PostTitleComponent} from './shared/post-title/post-title.component';
+import {SideBarComponent} from './shared/side-bar/side-bar.component';
+import {SubredditSideBarComponent} from './shared/subreddit-side-bar/subreddit-side-bar.component';
+import {VoteButtonComponent} from './shared/vote-button/vote-button.component';
+import {CreateSubredditComponent} from './subreddit/create-subreddit/create-subreddit.component';
+import {CreatePostComponent} from './post/create-post/create-post.component';
+import {ListSubredditsComponent} from './subreddit/list-subreddits/list-subreddits.component';
 import {EditorModule} from "@tinymce/tinymce-angular";
-import { ViewPostComponent } from './post/view-post/view-post.component';
+import {ViewPostComponent} from './post/view-post/view-post.component';
 import {UserProfileComponent} from "./auth/user-profile/user-profile.component";
 import {SignUpComponent} from "./auth/sign-up/sign-up.component";
 
 @NgModule({
-    declarations: [
-      AppComponent,
-      HeaderComponent,
-      SignUpComponent,
-      LoginComponent,
-      HomeComponent,
-      PostTitleComponent,
-      VoteButtonComponent,
-      SideBarComponent,
-      SubredditSideBarComponent,
-      CreateSubredditComponent,
-      CreatePostComponent,
-      ListSubredditsComponent,
-      ViewPostComponent,
-      UserProfileComponent
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SignUpComponent,
+    LoginComponent,
+    HomeComponent,
+    PostTitleComponent,
+    VoteButtonComponent,
+    SideBarComponent,
+    SubredditSideBarComponent,
+    CreateSubredditComponent,
+    CreatePostComponent,
+    ListSubredditsComponent,
+    ViewPostComponent,
+    UserProfileComponent
 
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        NgxWebstorageModule.forRoot(),
-        BrowserAnimationsModule,
-        ToastrModule.forRoot(),
-        FontAwesomeModule,
-        EditorModule
-    ],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: TokenInterceptor,
-            multi: true
-        }
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FontAwesomeModule,
+    EditorModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
 
-    ],
-    exports: [
-        PostTitleComponent
-    ],
-    bootstrap: [AppComponent]
+  ],
+  exports: [
+    PostTitleComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
