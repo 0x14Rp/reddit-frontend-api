779ed87803891d470ed56f85714c5fa481d59241
Rafael <elgondu@gmail.com>
2020-06-06 23:06:53 -0400

    add user component

---
+++ src/app/auth/user-profile/user-profile.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {PostModel} from "../../shared/post-model";
+import {CommentService} from "../../comment/comment.service";
+import {PostService} from "../../shared/post.service";
+import {ActivatedRoute} from "@angular/router";
+import {CommentPayload} from "../../comment/comment-payload";
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+  name: string;
+  posts: PostModel[];
+  comments: CommentPayload[];
+  postLength: number;
+  commentLength: number;
+
+  constructor(private activatedRoute: ActivatedRoute, private postService: PostService,
+              private commentService: CommentService) {
+    this.name = this.activatedRoute.snapshot.params.name;
+
+    this.postService.getAllPostsByUser(this.name).subscribe(data => {
+      this.posts = data;
+      this.postLength = data.length;
+    });
+    this.commentService.getAllCommentsByUser(this.name).subscribe(data => {
+      this.comments = data;
+      this.commentLength = data.length;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+}
