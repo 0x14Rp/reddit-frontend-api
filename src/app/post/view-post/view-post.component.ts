ce7265ddef4ae130014090789d6dad79f3721679
Rafael <elgondu@gmail.com>
2020-06-06 23:04:42 -0400

    add view post component

---
+++ src/app/post/view-post/view-post.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnInit} from '@angular/core';
+import {PostModel} from "../../shared/post-model";
+import {PostService} from "../../shared/post.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {throwError} from "rxjs";
+import {CommentService} from "../../comment/comment.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CommentPayload} from "../../comment/comment-payload";
+
+@Component({
+  selector: 'app-view-post',
+  templateUrl: './view-post.component.html',
+  styleUrls: ['./view-post.component.css']
+})
+export class ViewPostComponent implements OnInit {
+
+  postId: number;
+  post: PostModel;
+  commentForm: FormGroup;
+  commentPayload: CommentPayload;
+  comments: CommentPayload[];
+
+  constructor(private postService: PostService, private activateRoute: ActivatedRoute,
+              private commentService: CommentService, private router: Router) {
+    this.postId = this.activateRoute.snapshot.params.id;
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', Validators.required)
+    });
+    this.commentPayload = {
+      text: '',
+      postId: this.postId
+    };
+  }
+
+  ngOnInit(): void {
+    this.getPostById();
+    this.getCommentsForPost();
+  }
+
+  postComment() {
+    this.commentPayload.text = this.commentForm.get('text').value;
+    this.commentService.postComment(this.commentPayload).subscribe(data => {
+      this.commentForm.get('text').setValue('');
+      this.getCommentsForPost();
+    }, error => {
+      throwError(error);
+    })
+  }
+
+  private getPostById() {
+    this.postService.getPost(this.postId).subscribe(data => {
+      this.post = data;
+    }, error => {
+      throwError(error);
+    });
+  }
+
+  private getCommentsForPost() {
+    this.commentService.getAllCommentsForPost(this.postId).subscribe(data => {
+      this.comments = data;
+    }, error => {
+      throwError(error);
+    });
+  }
+
+}
